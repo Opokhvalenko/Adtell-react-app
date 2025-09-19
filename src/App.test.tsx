d3afe5a00c69c5d3a692e9dfc702808da740d85c
Oksana <oksanapokhvalenko@gmail.com>
2025-09-19 12:32:48 +0300

    check biome

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,12 +3,12 @@ import { MemoryRouter } from "react-router-dom";
 import App from "./App";
 
 it("renders header", async () => {
-  render(
-    <MemoryRouter initialEntries={["/"]}>
-      <App />
-    </MemoryRouter>,
-  );
+	render(
+		<MemoryRouter initialEntries={["/"]}>
+			<App />
+		</MemoryRouter>,
+	);
 
-  // findByText почекає, поки Suspense зникне і Header з'явиться
-  expect(await screen.findByText(/News App/i)).toBeInTheDocument();
+	// findByText почекає, поки Suspense зникне і Header з'явиться
+	expect(await screen.findByText(/News App/i)).toBeInTheDocument();
 });
